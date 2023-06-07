62b39683f62c5a995ca863c482e270135aa36a2c
DavitInjgia <69914940+DavitInjgia@users.noreply.github.com>
2023-06-07 10:51:21 +0400

    add-nav

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-<<<<<<< HEAD
 import ProductDetails from "./pages/ProductDetails";
-import './generalCSS/reset.css'
-=======
+import "./generalCSS/reset.css";
 
->>>>>>> 8c2189d722759cb7973aae9ec53aa13de4b3dce0
-
-import Header from './genericComponents/Header/Header';
-import './generalCSS/reset.css'
-import DropdownEmpty from './genericComponents/Header/components/UserInfo/Cart/components/components/DropdownEmpty';
+import Header from "./genericComponents/Header/Header";
+import "./generalCSS/reset.css";
+import DropdownEmpty from "./genericComponents/Header/components/UserInfo/Cart/components/components/DropdownEmpty";
 function App() {
   return (
-   <>
-<<<<<<< HEAD
-  <ProductDetails/>
-=======
-   <Header/>
-   <DropdownEmpty/>
->>>>>>> 8c2189d722759cb7973aae9ec53aa13de4b3dce0
-   </>
+    <>
+      <Header />
+      <ProductDetails />
+      {/* <DropdownEmpty/> */}
+    </>
   );
 }
 
