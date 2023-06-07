945893a99339987a481aa7fb7b5f86b2b2b50be5
DavitInjgia <69914940+DavitInjgia@users.noreply.github.com>
2023-06-07 12:45:53 +0400

    slider

--- src/App.js
+++ src/App.js
@@ -6,17 +6,11 @@ import './generalCSS/reset.css'
 import DropdownEmpty from './genericComponents/Header/components/UserInfo/Cart/components/components/DropdownEmpty';
 function App() {
   return (
-<<<<<<< HEAD
     <>
       {/* <Header /> */}
       <ProductDetails />
       {/* <DropdownEmpty/> */}
     </>
-=======
-   <>
-  <ProductDetails/>
-   </>
->>>>>>> 00266e78368230ba18bdd0d3718c8235a7c6fac4
   );
 }
 
--- src/pages/ProductDetails.js
+++ src/pages/ProductDetails.js
@@ -1,10 +1,6 @@
 import React from "react";
 import ProductDetailsTemplate from "../Modules/ProductDetails/ProductDetailsTemplate";
-<<<<<<< HEAD
-import ProductsMain from "../Modules/ProductDetails/ProductsMain";
-=======
 import Header from "../genericComponents/Header/Header";
->>>>>>> 00266e78368230ba18bdd0d3718c8235a7c6fac4
 
 function ProductDetails() {
   return (
@@ -12,12 +8,6 @@ function ProductDetails() {
       header={
         <div
           style={{ height: "112px", backgroundColor: "magenta", width: "100%" }}
-<<<<<<< HEAD
-        ></div>
-      }
-      main={
-       <ProductsMain/>
-=======
         >
           <Header/>
         </div>
@@ -26,7 +16,6 @@ function ProductDetails() {
         <div
           style={{ height: "787px", backgroundColor: "aqua", width: "100%" }}
         ></div>
->>>>>>> 00266e78368230ba18bdd0d3718c8235a7c6fac4
       }
     />
   );
