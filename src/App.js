b5598f2a68e7776ace3edc782dd68f174750137d
Mikheil-Barnaveli <barnavelimishiko@gmail.com>
2023-06-07 12:50:10 +0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ import Header from './genericComponents/Header/Header';
 import './generalCSS/reset.css'
 import DropdownEmpty from './genericComponents/Header/components/UserInfo/Cart/components/components/DropdownEmpty';
 function App() {
+
+  // ცვლილებები გავაკეთეთ
+
+
   return (
    <>
   <ProductDetails/>
