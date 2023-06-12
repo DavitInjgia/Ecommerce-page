07bc2776956a89a96bd7296724e114f9b06153b9
DavitInjgia <69914940+DavitInjgia@users.noreply.github.com>
2023-06-12 12:02:03 +0400

    added-products-details

--- src/Modules/ProductDetails/ProductsMain.js
+++ src/Modules/ProductDetails/ProductsMain.js
@@ -1,12 +1,19 @@
-import React from 'react'
-import SliderModal from './SliderModal/SliderModal'
-import './ProductsMail.css'
-function ProductsMain() {
+import React from "react";
+import SliderModal from "./SliderModal/SliderModal";
+import "./ProductsMail.css";
+import ProductInfo from "./ProductDetailsMain/components/ProductInfo/ProductInfo";
+function ProductsMain(props) {
   return (
     <div className="products-main-container">
-        <SliderModal/>
+      <SliderModal />
+      <ProductInfo
+        myObject={props.myObject}
+        data={props.data}
+        handleChangeQuantity={props.handleChangeQuantity}
+        handleAddItem={props.handleAddItem}
+      />
     </div>
-  )
+  );
 }
 
-export default ProductsMain
+export default ProductsMain;
